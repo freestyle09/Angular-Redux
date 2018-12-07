ddb541669b93cef323ee450466a4ee0536fd894e
freestyle09 <jozefrzadkosz09@gmail.com>
2018-12-07 14:59:58 +0100

    created angular app

---
+++ redux-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'redux-app';
+  age: number = 23;
+  isTrue: boolean = false;
+
+  reverseTrue = () => {
+    this.isTrue = !this.isTrue;
+  }
+}
