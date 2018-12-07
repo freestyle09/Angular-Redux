eaa14f1e7055bd022edfa2804ac8509c3761a03f
freestyle09 <jozefrzadkosz09@gmail.com>
2018-12-07 15:20:46 +0100

    added ngfor loop

--- redux-app/src/app/app.component.ts
+++ redux-app/src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'redux-app';
+  title = "redux-app";
   age: number = 23;
   isTrue: boolean = false;
 
   reverseTrue = () => {
     this.isTrue = !this.isTrue;
-  }
+  };
+
+  myArray: Array<object> = [
+    { name: "Arnold" },
+    { name: "Franek" },
+    { name: "Stanisław" }
+  ];
+
+
+
+
 }
